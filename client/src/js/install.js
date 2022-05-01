55e9207bf4fe32461c0260cf11375fa84483214e
Rachel <yarkony.rachel@gmail.com>
2022-05-01 01:03:50 -0500

    removed comments

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -10,7 +10,7 @@ window.addEventListener("beforeinstallprompt", (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
-  const promptEvent = window.deferredPromt;
+  const promptEvent = window.deferredPrompt;
 
   if (!promptEvent) {
     return;
