fb051e46cc73e28acd0fcad7b332aaa7ceffa624
Rachel <yarkony.rachel@gmail.com>
2022-05-01 00:08:02 -0500

    edits

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,6 +17,7 @@ module.exports = () => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
+
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
